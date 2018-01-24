c26c8440d0c77081b2208198e5c773728068fa60
stionic <levanquyen97@gmail.com>
2018-01-25 02:18:09 +0700

    Customize notifications icon

--- hooks/android_notification_icons.js
+++ hooks/android_notification_icons.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 var filestocopy = [{
-  "resources/android/icon/drawable-hdpi-icon.png":
+  "resources/stionic/notifications/drawable-hdpi-icon.png":
   "platforms/android/res/drawable-hdpi/ic_stat_onesignal_default.png"
 }, {
-  "resources/android/icon/drawable-mdpi-icon.png":
+  "resources/stionic/notifications/drawable-mdpi-icon.png":
   "platforms/android/res/drawable-mdpi/ic_stat_onesignal_default.png"
 }, {
-  "resources/android/icon/drawable-xhdpi-icon.png":
+  "resources/stionic/notifications/drawable-xhdpi-icon.png":
   "platforms/android/res/drawable-xhdpi/ic_stat_onesignal_default.png"
 }, {
-  "resources/android/icon/drawable-xxhdpi-icon.png":
+  "resources/stionic/notifications/drawable-xxhdpi-icon.png":
   "platforms/android/res/drawable-xxhdpi/ic_stat_onesignal_default.png"
 }, {
-  "resources/android/icon/drawable-xxxhdpi-icon.png":
+  "resources/stionic/notifications/drawable-xxxhdpi-icon.png":
   "platforms/android/res/drawable-xxxhdpi/ic_stat_onesignal_default.png"
 }];
 
