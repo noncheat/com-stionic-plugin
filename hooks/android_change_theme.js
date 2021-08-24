be249a35a4d405b6dea6c665ee57dddcf90a9f6b
noncheat <admin@noncheat.com>
2021-08-24 14:38:49 +0700

    Update for cordova-android@10

--- hooks/android_change_theme.js
+++ hooks/android_change_theme.js
@@ -15,6 +15,9 @@ var data = fs.readFileSync(manifest, 'utf8');
 var result = data.replace(
   'android:theme="@android:style/Theme.DeviceDefault.NoActionBar"',
   'android:theme="@style/Stionic"'
+).replace(
+  'android:theme="@style/Theme.AppCompat.NoActionBar"',
+  'android:theme="@style/Stionic"'
 );
 fs.writeFileSync(manifest, result, 'utf8');
 console.log('Stionic.com: Changed application theme');
